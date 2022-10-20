b761f375afb94801f316b7635c16b7d3f8ab8ad2
André Marques <andre.costa.marques@hotmail.com>
2022-10-20 08:11:56 -0300

    atualizacao do codigo (andre) para multiplicador, ta dandor problema na hora de printar

--- proj_01.asm
+++ proj_01.asm
@@ -114,20 +114,33 @@ menos proc
 
 menos endp
 
-mult proc           ; ta funcionando mas tem 2 problemas: printar numeros com mais de 1 casa decimal e receber input com mais de uma casa decimal
+mult proc ; ta funcionando mas tem 2 problemas: printar numeros com mais de 1 casa decimal e receber input com mais de uma casa decimal
 
-    mov cl,0 ;inicia o loop zerado
-    mov al,bh
-    sub bl,1 ;para o algoritmo funcionar a soma tem que ser feita x-1 vezes seguidas 
-    inicio_loop_mult:
-    add bh,al ;adiciona al(valor incial de bh) em bh
-    inc cl  ;aumenta o contador
-    cmp cl,bl   ;compara se o contador é do tamanho do multiplicador 
-    jl inicio_loop_mult ;se ele é menor a gnt comeca o loop de novo
-
-    mov cl,bh   ;movemos o produto da multiplicao para cl ser printado
-    mov bl,"+" ;se der pra inserir numeros NEGATIVOS <<< isso tem que mudar, colocar mesmo esquema de sub 
-    jmp print_mult 
+    mov ch,bh  ;mando o valor de bh(input multiplicando=const) para multiplicando = variavel
+    mov cl,bl  ;manda o valor de bl(input multiplicador=const) para multiplicador = variavel
+    mov bl,0    ;zero o contador
+    jmp primeira    ;a primeira vez que o codigo rodar ele vai pular direto para que o multiplicano nao da shift para esquerda
+    restart:    ;o loop recomeca
+    shl ch,1    ;shift de multiplicando para esquerda por 1 bit
+    inc bl ;transofrma bl em contador para loop
+    primeira:   ;pula pra primeira vez
+    ror cl,1 ;rotate de multiplicador para direita com o intuito de saber o valor de carry
+    jnc carry0
+    jc carry1
+    carry1:
+    add ch,bh ;q sofreu shift pra esquerda toda vez q o loop roda <<<<<<<<<<<<< testar com ADD
+    cmp bl,4 ;compara contador com 4 para saber se ele teria que reiniciar o loop ou printar
+    jnz restart ;se for diferente de 4, o loop vai recomecar e o contador vai ser adicionado
+    mov cl,ch ;mandando o valor de ch completo para cl ser printado
+    mov bl,"+"
+    jmp print   
+    carry0:
+    add ch,00h  ;no caso de carry0 a soma vai ser com 0 por conta de ser zerado
+    cmp bl,4  ;caso contador seja 4 ele vai printar direto
+    jnz restart
+    mov cl,ch
+    mov bl,"+"
+    jmp print
 
 mult endp
 
@@ -141,7 +154,6 @@ printar proc
 
     xor ch,ch ;comecamos a divisao do resultado para que possamos imprimir 2 valores
     mov ax,cx
-    print_mult:
     mov ch,10
     div ch ;al = cosciente ah = resto
     
@@ -166,7 +178,7 @@ printar proc
     mov dl,ch ;printa o resultado
     int 21h
     
-    jmp exit
+    jmp start
 printar endp
 
 end main 
